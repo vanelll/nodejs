512d401efe13d43eb2d3350aed962274654a6057
seyeon <tltptltptltp@gmail.com>
2020-04-04 21:29:58 +0900

    init geo

---
+++ hw/geo/src/index.js
@@ -0,0 +1,3 @@
+import "./styles.css";
+
+const API_URL = "http://ip-api.com/json/";
--- hw/hw15/src/index.js
+++ hw/hw15/src/index.js
@@ -1,3 +1,13 @@
 import "./styles.css";
 
 const API_URL = "https://api.coinpaprika.com/v1/tickers";
+
+
+/**
+ * country
+ * coutry code
+ * region name
+ * city
+ * lat/long
+ * 
+ */
