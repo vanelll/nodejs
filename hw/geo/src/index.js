d54115f0530a3c2c7b4475e479952376dadaad49
seyeon <tltptltptltp@gmail.com>
2020-04-04 23:00:50 +0900

    fetch, html작성

--- hw/geo/src/index.js
+++ hw/geo/src/index.js
@@ -1,3 +1,16 @@
 import "./styles.css";
 
+/**
+ * 
+🚀Call this http://ip-api.com/json/
+ call url
+show me my location
+
+https://developer.mozilla.org/ko/docs/Web/API/Response
+ */
+
 const API_URL = "http://ip-api.com/json/";
+
+const geo = document.querySelector(".geo");
+
+fetch("API_URL").then(res => res.json());
