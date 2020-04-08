5bc42bb48e6185babcba4de250a58b0c24114469
seyeon <tltptltptltp@gmail.com>
2020-04-08 21:46:06 +0900

    finit array_hw

--- react/array_basic/src/index.js
+++ react/array_basic/src/index.js
@@ -1,20 +1,33 @@
-class ArrayUtilities {
-  /* Your magic here */
+/*
+class !어레이! {
+  static 더하기제로 = array => {
+    const x = array.map(x => x * 10);
+    return x;
+  };
+  static 50보다큰 수 = array => {
+    const x = array.filter(x => x > 50);
+    return x;
+  };
+  static 첫번째지우기 = array => {
+    const x = array.slice(1);
+    return x;
+  };
+  static 다더하기 = x => {
+    const reducer = (acc, value) => acc + value;
+    return x.reduce(reducer);
+  };
+  static 숫자쪼개기 = x => {
+    const xx = x.toString().split("");
+    return xx;
+  };
 }
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+output: 
 
-const addZeros = ArrayUtilities.addZeros(numbers);
-console.log(addZeros);
+[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+[60, 70, 80, 90, 100]
+[70, 80, 90, 100]
+340
+["3", "4", "0"]
 
-const moreThanFifty = ArrayUtilities.moreThanFifty(addZeros);
-console.log(moreThanFifty);
-
-const noFirst = ArrayUtilities.removeFirst(moreThanFifty);
-console.log(noFirst);
-
-const sumAll = ArrayUtilities.sumAll(noFirst);
-console.log(sumAll);
-
-const divided = ArrayUtilities.divide(sumAll);
-console.log(divided);
+*/
