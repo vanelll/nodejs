3ca43ad6d79a4b1066dfda83154de7fd42e66cd6
seyeon <tltptltptltp@gmail.com>
2020-04-09 22:04:22 +0900

    header

--- react/app_lab/src/Components/Header.js
+++ react/app_lab/src/Components/Header.js
@@ -1,3 +1,25 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
-export default () => <header>{/* Magic Here */}</header>;
+export default () => (
+  <header>
+    {
+      <ul>
+        <li>
+          <Link to="/">Prices</Link>
+        </li>
+        <li>
+          <Link to="/ex">Exchanges</Link>
+        </li>
+        <li>
+          <Link to="/coin">Coins</Link>
+        </li>
+      </ul>
+    }
+  </header>
+);
+
+/**
+ * https://reacttraining.com/react-router/web/example/url-params
+ * link
+ */
