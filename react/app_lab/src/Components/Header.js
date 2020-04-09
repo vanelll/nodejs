c3099af9d8aea1174c5514494eaf64736d0793ff
seyeon <tltptltptltp@gmail.com>
2020-04-09 22:52:17 +0900

    compnents

--- react/app_lab/src/Components/Header.js
+++ react/app_lab/src/Components/Header.js
@@ -1,21 +1,19 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default () => (
   <header>
-    {
-      <ul>
-        <li>
-          <Link to="/">Prices</Link>
-        </li>
-        <li>
-          <Link to="/ex">Exchanges</Link>
-        </li>
-        <li>
-          <Link to="/coin">Coins</Link>
-        </li>
-      </ul>
-    }
+    <ul>
+      <li>
+        <Link to="/">🏷</Link>
+      </li>
+      <li>
+        <Link to="/exchanges">🔁</Link>
+      </li>
+      <li>
+        <Link to="/coins">💰</Link>
+      </li>
+    </ul>
   </header>
 );
 
--- react/app_lab/src/Components/Router.js
+++ react/app_lab/src/Components/Router.js
@@ -1,6 +1,19 @@
 import React from "react";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Route, Redirect } from "react-router-dom";
+
+import Coins from "../Screens/Coins";
+// 나머지도 임포트, 헤더도 
+
+import Header from "./Header";
 
 export default () => {
-  return <Router>{/* Magic Here */}</Router>;
+  return (
+    <Router>
+      <Header />
+      <Route path="/" exact component={🏷} />
+      <Route path="/코인" exact component={💰} />
+      <Route path="/익스" exact component={🔁} />
+      <Redirect from="*" to="/" />
+    </Router>
+  );
 };
