07ebe3243e332796cec74312e1929bdbb12c32e8
seyeon <tltptltptltp@gmail.com>
2020-04-12 22:47:08 +0900

    style-components

--- react/app_lab/src/Components/Header.js
+++ react/app_lab/src/Components/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+
+//style-components: local
 const Header = styled.header`
   margin-bottom: 30px;
 `;
