00458ac05f4a2bffc47b9ea24b4c709917543952
seyeon <tltptltptltp@gmail.com>
2020-06-06 22:57:51 +0900

    startRecordingVideo

--- src/assets/js/videoRecorder.js
+++ src/assets/js/videoRecorder.js
@@ -2,19 +2,43 @@ const recorderContainer= document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview= document.getElementById("jsVideoPreview");
 
-const startRecording= async () =>{
+let streamObject;
+
+const handleVideoData= (e)=> {
+    //console.log(e);
+}
+
+//stream에서 가진 video 녹화
+const startRecording = () =>{
+    const videoRecorder= new MediaRecorder(streamObject);
+    //console.log(videoRecorder);
+    videoRecorder.start();
+    //videoRecorder.start(1000) 매 초마다 데이터 줌
+    //start recording
+    videoRecorder.addEventListener("dataavailable", handleVideoData);
+};
+
+const stopRecording= () =>{
+
+};
+
+const getVideo= async () =>{
     try{
         const stream= await navigator.mediaDevices.getUserMedia({
             audio:true,
             video:true
         });
         videoPreview.srcObject = stream;
+        streamObject=stream;
+        startRecording();
         videoPreview.play();
+        recordBtn.innerHTML="stop recording";
     }catch(error){
         recordBtn.innerHTML= "cant record";
-        recordBtn.removeEventListener("click",startRecording);
+    }finally {
+        recordBtn.removeEventListener("click",getVideo)
     }
-}
+};
 
 function init(){
     recorderContainer.addEventListener("click", startRecording);
