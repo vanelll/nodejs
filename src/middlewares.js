725fd504149a0575bcaecb21d1c43000462b2484
seyeon <tltptltptltp@gmail.com>
2020-06-09 23:15:18 +0900

    .

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,5 +1,8 @@
 import routes from "./routes";
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
 
 const multerVideo = multer({dest: "uploads/videos/"});
 
