429740889243db7ee45c9c3bde60b34739d84df7
seyeon <tltptltptltp@gmail.com>
2020-06-13 23:51:59 +0900

    multer3s aws

--- src/middlewares.js
+++ src/middlewares.js
@@ -3,8 +3,27 @@ import multer from "multer";
 import multerS3 from "multer-s3";
 import aws from "aws-sdk";
 
+const s3= new aws.S3({
+    accessKeyId:process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_PRIVATE_KEY,
+    region: "ap-northeast-1"
+});
 
-const multerVideo = multer({dest: "uploads/videos/"});
+const multerVideo = multer({
+    storage:multerS3({
+        s3,
+        acl: "public-read",
+        bucket: "wetube/video"
+    })
+   });
+
+const multerAvatar = multer({
+    storage:multerS3({
+        s3,
+        acl: "public-read",
+        bucket: "wetube/avatar"
+    })
+    });
 
 export const uploadVideo = multerVideo.single("videoFile");
 export const uploadAvatar = multerAvatar.single("avatar");
@@ -12,7 +31,7 @@ export const uploadAvatar = multerAvatar.single("avatar");
 export const localMiddleware=(req,res,next) =>{
     res.locals.siteName= 'wetube';
     res.locals.routes= routes;
-    res.locals.loggedUser=req.user || null;
+    res.locals.oggedUser=req.user || null;
     next();
 };
 
