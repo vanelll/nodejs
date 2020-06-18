139d85e29012288b1c74cf432a694a11e437e573
seyeon <tltptltptltp@gmail.com>
2020-06-18 22:53:08 +0900

    setting

--- guess-mine/src/server.js
+++ guess-mine/src/server.js
@@ -1,7 +1,13 @@
+import {join} from "path";
 import express from "express";
+import socketIO from "socket.io";
 
 const PORT=4000;
 const app = express();
+app.set("view engine","pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname,"static")));
+app.get("/", (req,res)=> res.render("home"));
 
 const handleListening= ()=> console.log(`👽 hello : http://localhost:${PORT}`);
 
