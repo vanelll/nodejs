3ca79ff817c6e601eb9cfa8a7248d346568ce15a
seyeon <tltptltptltp@gmail.com>
2020-07-01 23:46:24 +0900

    ㅠㅠ...

--- guess-mine/src/gulpfile.babel.js
+++ guess-mine/src/gulpfile.babel.js
@@ -1,10 +1,15 @@
 import gulp from "gulp";
 import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
+
+sass.compiler= require("node-sass");
 
 const paths= {
     styles:{
         src: "assets/scss/styles.scss",
-        dest: "src/static/styles"
+        dest: "src/static/styles",
+        watch: "/assets/scss/**/*.scss"
     }
 }
 
@@ -12,6 +17,17 @@ export function styles(){
     return gulp
     .src(paths.styles.src)
     .pipe(sass())
+    .pipe(
+        autoprefixer({
+            browsers: ["last 2 versions"],
+            cascade: false
+        })
+    )
     .pipe(gulp.dest(paths.styles.dest));
     
+    }
+
+    function watchFiles(){
+        gulp.watch();
+
     }
