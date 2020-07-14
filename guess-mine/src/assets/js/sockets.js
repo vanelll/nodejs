cc9b60d11a10345508cd9f3a1a1d62af3fd30c8d
seyeon <tltptltptltp@gmail.com>
2020-07-14 21:39:19 +0900

    paint stroke

--- guess-mine/src/assets/js/sockets.js
+++ guess-mine/src/assets/js/sockets.js
@@ -1,6 +1,6 @@
 import { handleNewUser, handleDisconnected } from "./notifications";
 import { handleNewMessage } from "./chat";
-import { handleBeganPath } from "./paint";
+import { handleBeganPath, handleStrokedPath } from "./paint";
 
 let socket = null;
 
@@ -15,5 +15,5 @@ export const initSockets = (aSocket) => {
     aSocket.on(events.disconnected, handleDisconnected);
     aSocket.on(events.newMsg, handleNewMessage);
     aSocket.on(events.beganPath, handleBeganPath);
-    aSocket.on(events.)
+    aSocket.on(events.strokedPath, handleStrokedPath);
 };
