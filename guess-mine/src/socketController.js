18f7da97c65141c7e47eeff4110b5bb8b5de7598
seyeon <tltptltptltp@gmail.com>
2020-07-19 23:51:50 +0900

    .

--- guess-mine/src/socketController.js
+++ guess-mine/src/socketController.js
@@ -69,14 +69,5 @@ export default socketController;
 
 
 /*
-{
-    socket.on("newMassage", message=> {
-     socket.broadcast.emit("messageNotif", {
-         message, 
-         nickname: socket.nickname || "anon"});
-    });
-    socket.on("setNickName", ({nickname})=> {
-        socket.nickname= nickname;
-        //socket = obj, socket.potato=5 이런거도 가능
-    })
+
  }*/
