7ba4c5f845754cdbebe89b3776f2f404904968d3
seyeon <tltptltptltp@gmail.com>
2020-08-15 23:14:52 +0900

    routes

---
+++ react/nomflix/src/Components/Router.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { HashRouter as Router, Route ,Redirect,
+Switch} from "react-router-dom";
+import Home from "Routes/Home";
+import Search from "Routes/Search";
+import TV from "Routes/TV";
+
+export default () =>(
+    <Router> <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/search" component={Search} />
+        <Route path="/tv" component={TV} />
+        <Redirect from="*" to="/" />
+        </Switch>
+    </Router>
+);
--- react/nomflix/src/index.js
+++ react/nomflix/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Components/App';
 
-ReactDOM.render(
-    <App />,
+ReactDOM.render(<App />,
   document.getElementById('root')
 );
