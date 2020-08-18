9d869efe34d4f7d1ccb844aac741873939961655
seyeon <tltptltptltp@gmail.com>
2020-08-18 19:56:06 +0900

    detail presenter

--- react/nomflix/src/Routes/Detail/DetailContainer.js
+++ react/nomflix/src/Routes/Detail/DetailContainer.js
@@ -1,12 +1,21 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
+import { moviesApi, tvApi } from "../../api";
 
 export default class extends React.Component{
-    state={
-        result:null,
-        error: null,
-        loading: true
-    };
+    constructor(props){
+        super(props);
+        const {
+            location : { pathname }
+        } = props;
+        this.state={
+            result:null,
+            error: null,
+            loading: true,
+            isMovie: pathname.includes("/movie/")
+        };
+    }
+    
 
     async componentDidMount(){
         const { match: 
@@ -15,11 +24,23 @@ export default class extends React.Component{
             },
             history : {push}
             } = this.props;
+            const { isMovie } = this.state;
         const parsedId= parseInt(id);
         if(isNaN(parsedId)){
             return push("/");
         }
-        
+        let result;
+        try{
+            if(isMovie){
+               ({ data: result }= await moviesApi.movieDetail(parsedId));
+            }else {
+                ({ data: result }= await tvApi.showDetail(parsedId));
+            }
+        }catch{
+            this.setState({ error: "can't find anything. "});
+        }finally{
+            this.setState({ loading: false, result });
+        }
             
     }
 
