4b3014e561f514dc36850c2d7fe2e0fa3ffaeb01
seyeon <tltptltptltp@gmail.com>
2020-08-18 19:09:53 +0900

    container,presenter

--- react/nomflix/src/Components/Router.js
+++ react/nomflix/src/Components/Router.js
@@ -4,6 +4,7 @@ Switch} from "react-router-dom";
 import Home from "Routes/Home";
 import Header from "Components/Header";
 import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 import TV from "Routes/TV";
 
 export default () =>(
@@ -14,6 +15,8 @@ export default () =>(
         <Route path="/" exact component={Home} />
         <Route path="/search" component={Search} />
         <Route path="/tv" component={TV} />
+        <Route path= "/movie/:id" component={Detail}/>
+        <Route path= "/show/:id" component={Detail}/>
         <Redirect from="*" to="/" />
         </Switch>
         </>
---
+++ react/nomflix/src/Routes/Detail/DetailContainer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import DetailPresenter from "./DetailPresenter";
+
+export default class extends React.Component{
+    state={
+        result:null,
+        error: null,
+        loading: true
+    };
+
+    async componentDidMount(){
+        const { match: 
+            { params : 
+                { id }
+            },
+            history : {push}
+            } = this.props;
+        const parsedId= parseInt(id);
+        if(isNaN(parsedId)){
+            return push("/");
+        }
+        
+            
+    }
+
+    render() {
+        const { result
+            ,error, loading} =this.state;
+         return (<DetailPresenter 
+            result={result}
+            error= {error}
+            loading ={loading}
+        />
+         );
+        }
+}
